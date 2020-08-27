5da512d5688488ad23310ae594068713899a3551
jennakimsd <jennakimsd@gmail.com>
2020-08-26 20:01:03 -0700

    I did Insertion Sorter, Quick Sorter, and Merge Sorter

--- src/_00_Sorting_Algorithms/_01_SwappingDigits.java
+++ src/_00_Sorting_Algorithms/_01_SwappingDigits.java
@@ -25,7 +25,8 @@ class _01_SwappingDigits {
 	//1. Complete the method so that the first two elements of the array are swapped
 	public static void swapArrayOfTwo(int[] arr) {
 		int first = arr[0];
-		arr[0] = arr[1];
+		int second = arr[1]; 
+		arr[0] = second; 
 		arr[1] = first;
 	}
 	
@@ -33,13 +34,22 @@ class _01_SwappingDigits {
 	//   Iterate through the array and when you find two elements that are out
 	//   of order, swap them. Repeat this until the array is in order.
 	public static void sortIntArray(int[] arr) {
-		
-	}
+		for (int i = 0; i < arr.length-1; i++) 
+            for (int j = 0; j < arr.length-i-1; j++) {
+                if (arr[j] > arr[j+1]) 
+                { 
+                    int temp = arr[j]; 
+                    arr[j] = arr[j+1]; 
+                    arr[j+1] = temp; 
+                } 
+            }
+		}
 	
 	//3. Complete the method so that it finds the middle number in the array.
 	//   *Hint* it helps to sort it first.
 	//   *Double Hint* Use the method you already wrote in step 2 to sort it
 	public static int findMiddle(int[] arr) {
-		return 0;
+		sortIntArray(arr);
+		return arr[arr.length/2];
 	}
 }
